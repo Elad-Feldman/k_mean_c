fb217c74b8324f30cb17aa9d258e3a6d6e3ec58c
Elad-Feldman <73156908+Elad-Feldman@users.noreply.github.com>
2021-04-29 17:39:12 +0300

    Add files via upload
    v3

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 // run by gcc main.c && gcc  -o main main.c && main 3 200 < input1.txt
+
 void init_cluster_values(float** dot_list,float** cluster_list, int k, int d)
 {
     for(int i = 0; i < k; i++)
@@ -14,40 +15,51 @@ void init_cluster_values(float** dot_list,float** cluster_list, int k, int d)
     }
 
 }
-float find_distance(float dot[],float center[], int d){
-    float dis = 0;
-    for (int i = 0; i < d; i++)
-        dis += (dot[i] - center[i]) * (dot[i] - center[i]);
-    return  dis;
-
-    }
-
 void init_dots_list(float** dot_list, int start,int end,int d){
     for(int i = start; i < end; i++)
         dot_list[i] = (float*) calloc(d,sizeof(float));
 
 
 }
-void print_Arr(float arr[], int d) {
-    printf("[");
-    for (int i = 0; i < d; i++) {
-        printf("%0.2f,", arr[i]);
 
-    }
-    printf("],\n ");
-}
-void print_matrix(float** mat,int n,int d){
-    printf("\n========================\n");
-    printf("[");
-    for (int i = 0; i < n; i++) {
-        print_Arr(mat[i],d);
-
-
-    }
-    printf("]");
-    printf("\n========================\n");
+float find_distance(float *dot, float *center, int d){
+    float dis = 0;
+    for (int i = 0; i < d; i++)
+        dis += (dot[i] - center[i]) * (dot[i] - center[i]);
+    return  dis;
 
 }
+
+int get_index_of_closest_cluster(float* dot, float** cluster_list, int d, int k ) {
+    int j = 0;
+    float min_dis = find_distance(dot, cluster_list[0], d);
+    float tmp_dis;
+    for (int i = 1; i < d; i++) {
+        tmp_dis = find_distance(dot, cluster_list[i], d);
+        if (tmp_dis >= min_dis) {
+            min_dis = tmp_dis;
+            j = i;
+        }
+    }
+    return j;
+}
+
+void update_cluster_center(float* dot, float * center,int cluster_size,int d,int sign) {
+    if (cluster_size+sign==0)
+        printf("error");
+    float temp[d];
+    for (int i = 0; i < d; i++)
+        temp[i] = (center[i] * (cluster_size));
+    for (int i = 0; i < d; i++){
+        temp[i] += (dot[i]*sign);
+        center[i] = temp[i] / (cluster_size+sign);
+
+        }
+}
+
+
+
+
 float *string_to_floats(char line[],float arr[],int d )
 {
     int i = 0;
@@ -80,7 +92,34 @@ int count_dim(char line[])
     }
     return count;
 }
+void print_Arr_int(int* arr, int d) {
+    printf("[");
+    for (int i = 0; i < d; i++) {
+        printf("%d,", arr[i]);
 
+    }
+    printf("],\n ");
+}
+void print_Arr(float* arr, int d) {
+    printf("[");
+    for (int i = 0; i < d; i++) {
+        printf("%0.2f,", arr[i]);
+
+    }
+    printf("],\n ");
+}
+void print_matrix(float** mat,int n,int d){
+    printf("\n========================\n");
+    printf("[");
+    for (int i = 0; i < n; i++) {
+        print_Arr(mat[i],d);
+
+
+    }
+    printf("]");
+    printf("\n========================\n");
+
+}
 
 int main(int argc, char* argv[]) {
     int k, max_iter=200;
@@ -99,54 +138,98 @@ int main(int argc, char* argv[]) {
 
 
     scanf("%s", line);
-    int d = count_dim(line)+1;
-    int n=500;
+    int d = count_dim(line) + 1;
+    int n = 500;
    ;
     // initialize dot list.size=n and cluster list.size = k
     float** dot_list = (float **)malloc(n *sizeof(float*));
-    init_dots_list(dot_list,0,n,d);
+    init_dots_list(dot_list, 0, n, d);
 
     float** cluster_list = (float **)malloc(k *sizeof(float*));
-    init_dots_list(cluster_list,0,k,d);
+    init_dots_list(cluster_list, 0, k, d);
 
 
 
 
-
+     //load dots into list
     do{
         string_to_floats(line, dot_list[count], d);
         count++;
         if (count==n){
             n =n * 2;
             dot_list = (float **) realloc(dot_list, n * sizeof(dot_list[0]));
-            init_dots_list(dot_list,n/2,n,d);
+            init_dots_list(dot_list,n/2, n, d);
         } // double the size of array if needed
     } while ((scanf("%s", line)) == 1);
 
-    printf("Summery:Got %d dots, each dot have %d values \n",count,d);
+    printf("Summery:Got %d dots, each dot have %d values \n", count, d);
     dot_list = (float **)realloc(dot_list,count *sizeof(float *));
-
+    n = count;
     // initialize loctions ans cluster's size
-    int dot_at[count];
-    int move_dot_to[count];
-    int cluster_size[count];
-    for(int i = 0; i < count; i++){
-        if (i<k){
-            dot_at[i]=i;
-            cluster_size[i] = 1;
-        }
-        else
-            dot_at[i]=-1;
+    int dot_at[n];
+    int move_dot_to[n];
+    int cluster_size[k];
+    for(int i = 0; i < n; i++){
+        dot_at[i]=-1;
         move_dot_to[i] = 0 ;
+    }
+    for(int i = 0; i < k; i++) {
+        dot_at[i] = i;
+        cluster_size[i] = 1;
+
     }
 
 
 
-    print_matrix(dot_list,count,d);
-    init_cluster_values(dot_list,cluster_list,k,d);
-    print_matrix(cluster_list,k,d);
+
+  //  print_matrix(dot_list, count, d);
+
+    init_cluster_values(dot_list, cluster_list, k, d);
 
 
+    int is_a_cluster_changed=1;
+    int count_iter = 0;
+
+    float* cluster;
+    float * dot;
+    while (count_iter<max_iter && is_a_cluster_changed) {
+        is_a_cluster_changed = 0;
+        count_iter++;
+        for (int i = 0; i < n; i++) //find nearest clusters
+            move_dot_to[i] = get_index_of_closest_cluster(dot_list[i], cluster_list, d, k);
+
+        for (int j = 0; j < n; j++) {// update clusters
+
+            if (dot_at[j] == -1) {
+                dot_at[j] = move_dot_to[j];
+                update_cluster_center(dot_list[j], cluster_list[move_dot_to[j]], cluster_size[move_dot_to[j]], d,
+                                      1); //add dot to center
+                //   printf("updated dot %i, into cluster %d\n ",i, move_dot_to[i]);
+                // print_Arr(cluster_list[move_dot_to[i]],d);
+                cluster_size[move_dot_to[j]]++;
+                is_a_cluster_changed = 1;
+            } else {
+                if (dot_at[j] != move_dot_to[j]) {
+
+                    update_cluster_center(dot_list[j], cluster_list[dot_at[j]], cluster_size[dot_at[j]], d,-1); //remove dot from center
+                    update_cluster_center(dot_list[j], cluster_list[move_dot_to[j]], cluster_size[move_dot_to[j]], d,1); //add dot to center
+                    cluster_size[dot_at[j]]--;
+                    cluster_size[move_dot_to[j]]++;
+                    //  printf("move dot %i, from cluster %d to cluster %d \n",i,dot_at[i] ,move_dot_to[i]);
+                    dot_at[j] = move_dot_to[j];
+                    is_a_cluster_changed = 1;
+                }
+            }
+        }
+    }
+
+    print_matrix(cluster_list, k, d);
+
+
+
+
+
+    /// free memory
     for(int i = 0; i < count; i++){
         free(dot_list[i]);
         if (i<k)
